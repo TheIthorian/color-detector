856c01f37be6ac0c4e3a988da75282b97115d25f
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-08 17:59:51 +0100

    tests: fix sobel filter tests

--- src/__tests__/sobelFilter.test.js
+++ src/__tests__/sobelFilter.test.js
@@ -1,15 +1,43 @@
 import { PixelIterator, calculateSobelFactor, sobelFilter } from '../filters.js';
 
-describe('test desc', () => {
-    it('works', () => {
+describe('sobelFilter', () => {
+    describe('pixel iterations', () => {
+        /**
+         * [10 20]
+         * [30 40]
+         */
         const pixels = [10, 10, 10, 10, 20, 20, 20, 20, 30, 30, 30, 30, 40, 40, 40, 40];
-        const pixelIterator = new PixelIterator(pixels);
-        const height = 4;
-        const width = 4;
-        const sobelFactor = calculateSobelFactor(0, height, width, pixelIterator);
+        const height = 2;
+        const width = 2;
 
-        expect(sobelFactor).toBe(
-            10 * 1 + 10 * 0 + 20 * -1 + 2 * 10 + 10 * 0 + 20 + -2 + 30 * 1 + 30 * 0 + -1 * 40
-        );
+        it('correctly iterates over the adjacent elements for the first element', () => {
+            const pixelIterator = new PixelIterator(pixels);
+            const sobelFactor = calculateSobelFactor(0, height, width, pixelIterator);
+
+            const expectedSum = [
+                [10 * 1 + 10 * 0 + 20 * -1],
+                [10 * 2 + 10 * 0 + 20 * -2],
+                [30 * 1 + 30 * 0 + 40 * -1],
+            ]
+                .map(arr => arr[0])
+                .reduce((prev, curr) => prev + curr, 0);
+
+            expect(sobelFactor).toBe(expectedSum);
+        });
+
+        it('correctly iterates over the adjacent elements for the last element', () => {
+            const pixelIterator = new PixelIterator(pixels);
+            const sobelFactor = calculateSobelFactor(3, height, width, pixelIterator);
+
+            const expectedSum = [
+                [10 * 1 + 20 * 0 + 20 * -1],
+                [30 * 2 + 40 * 0 + 40 * -2],
+                [30 * 1 + 40 * 0 + 40 * -1],
+            ]
+                .map(arr => arr[0])
+                .reduce((prev, curr) => prev + curr, 0);
+
+            expect(sobelFactor).toBe(expectedSum);
+        });
     });
 });
