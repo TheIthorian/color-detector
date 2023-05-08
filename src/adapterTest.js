45b44981915a6e3b089b309d9d4b72bbd94e7c9e
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-08 15:25:40 +0100

    feat: adjust adapter test settings

--- src/adapterTest.js
+++ src/adapterTest.js
@@ -1,12 +1,13 @@
-import { invertFilter, mirrorY } from './filters.js';
+import { grayScale, greenScreen, invertFilter, mirrorFilter } from './filters.js';
+import { Logger } from './log.js';
 import { CanvasVideoOutput, VideoAdapter, VideoFilter } from './video.js';
 
 window.addEventListener('load', test);
 
 async function test() {
     const outputCanvasElement = document.querySelector('#output');
-    outputCanvasElement.setAttribute('width', 500);
-    outputCanvasElement.setAttribute('height', 500);
+    outputCanvasElement.setAttribute('width', 600);
+    outputCanvasElement.setAttribute('height', 600);
 
     const videoStream = await navigator.mediaDevices.getUserMedia({
         audio: false,
@@ -14,13 +15,15 @@ async function test() {
     });
 
     const videoAdapter = new VideoAdapter({
-        xResolution: 500,
-        yResolution: 500,
+        xResolution: 600,
+        yResolution: 600,
     });
-    const videoOutput = new CanvasVideoOutput(outputCanvasElement, { frameRate: 100 });
+    const videoOutput = new CanvasVideoOutput(outputCanvasElement, { frameRate: 24 });
 
+    const logger = new Logger(document.getElementById('log-output'));
     videoAdapter.connectInput(videoStream);
-    videoAdapter.connectOutput(invertFilter()).connectOutput(videoOutput);
+    videoAdapter.connectOutput(grayScale()).connectOutput(videoOutput);
+    // videoAdapter.connectOutput(videoOutput);
 
     console.log(videoAdapter.getVideoGraph());
 
