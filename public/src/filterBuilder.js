f646632ed59a4500de5640700637ed3e5b57e1c6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-10 20:41:51 +0100

    feat: add FilterBuilder

---
+++ public/src/filterBuilder.js
@@ -0,0 +1,37 @@
+import {
+    invertFilter,
+    mirrorFilter,
+    greenScreen,
+    sobelFilter,
+    grayScale,
+    blackAndWhiteFilter,
+} from './filters';
+
+export class FilterBuilder {
+    static FILTERS = {
+        invertFilter,
+        mirrorFilter,
+        greenScreen,
+        sobelFilter,
+        grayScale,
+        blackAndWhiteFilter,
+    };
+
+    static fromArray(arr) {
+        let head;
+        let prev, curr;
+
+        for (const filterName of arr) {
+            const filterConstructor = FilterBuilder.FILTERS[filterName];
+            if (!filterConstructor) continue;
+
+            curr = filterConstructor();
+            if (!head) head = curr;
+
+            prev?.connectOutput(curr);
+            prev = curr;
+        }
+
+        return head;
+    }
+}
