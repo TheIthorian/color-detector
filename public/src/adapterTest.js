ba1ca1cedf3dfc75968089a041d5d3d5f3d3e8da
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-10 22:23:18 +0100

    feat: use FilterBuilder to dynamically select filters

--- public/src/adapterTest.js
+++ public/src/adapterTest.js
@@ -1,10 +1,4 @@
-import {
-    blackAndWhiteFilter,
-    grayScale,
-    invertFilter,
-    mirrorFilter,
-    sobelFilter,
-} from './filters.js';
+import { FilterBuilder } from './filterBuilder.js';
 import { Logger } from './log.js';
 import { CanvasVideoOutput, VideoAdapter, VideoFilter } from './video.js';
 
@@ -28,15 +22,39 @@ async function test() {
 
     const logger = new Logger(document.getElementById('log-output'));
     videoAdapter.connectInput(videoStream);
-    videoAdapter
-        // .connectOutput(invertFilter())
-        .connectOutput(grayScale())
-        // .connectOutput(mirrorFilter())
-        .connectOutput(blackAndWhiteFilter())
-        // .connectOutput(sobelFilter())
-        .connectOutput(videoOutput);
+
+    const optionsContainer = document.querySelector('#options');
+
+    for (const element of optionsContainer.childNodes) {
+        if (element.type !== 'checkbox') continue;
+
+        element.addEventListener('change', e => {
+            // Find all enabled filters
+            const filters = [];
+            for (const checkbox of optionsContainer.childNodes) {
+                if (checkbox.checked) filters.push(checkbox.value);
+            }
+            console.log('newFilters:', filters);
+
+            // Just connect output
+            if (!filters.length) {
+                videoAdapter.connectOutput(videoOutput);
+                return;
+            }
+
+            // Disconnect existing filters
+            videoAdapter.getVideoGraph().forEach(node => node.disconnectOutput());
+
+            const head = FilterBuilder.fromArray(filters);
+            const graph = head.getVideoGraph();
+            const tail = graph[graph.length - 1];
+            videoAdapter.connectOutput(head);
+            tail.connectOutput(videoOutput);
+        });
+    }
+
+    videoAdapter.connectOutput(videoOutput);
+    videoOutput.display();
 
     console.log(videoAdapter.getVideoGraph());
-
-    videoOutput.display();
 }
